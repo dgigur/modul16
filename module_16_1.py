b32636fc873bf09893c2f63069f22b06eaa7f6e5
dgigur <dgigurda005@mail.ru>
2024-12-13 18:19:34 +0300

    Замена возвращаемого словаря на строку

--- module_16_1.py
+++ module_16_1.py
@@ -4,21 +4,25 @@ app = FastAPI()
 
 
 @app.get('/')
-async def main_paige() -> dict:
-    return {'message': 'Главная страница'}
+async def main_paige() -> str:
+    return 'Главная страница'
+    #return {'message': 'Главная страница'}
 
 
 @app.get('/user/admin')
-async def admin_enter() -> dict:
-    return {'message': 'Вы вошли как администратор'}
+async def admin_enter() -> str:
+    return 'Вы вошли как администратор'
+    #return {'message': 'Вы вошли как администратор'}
 
 
 @app.get('/user/{user_id}')
-async def user_enter(user_id) -> dict:
-    return {'message': f'Вы вошли как пользователь № {user_id}'}
+async def user_enter(user_id) -> str:
+    return f'Вы вошли как пользователь № {user_id}'
+    #return {'message': f'Вы вошли как пользователь № {user_id}'}
 
 
 @app.get('/user')
-async def user_info(username: str, age: int) -> dict:
-    return {'User': username, "Age": age}
+async def user_info(username: str, age: int) -> str:
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
+    #return {'User': username, "Age": age}
 
